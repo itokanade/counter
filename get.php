8b1ac4b4c5a7dcf8d7ecd73264b1c19b225c3aca
Ito Kanade <kanade.ito01069@gmail.com>
2020-06-08 15:06:51 +0900

    1st commit

---
+++ get.php
@@ -0,0 +1,35 @@
+<?php
+
+define('DATA_FILE', 'data.txt');
+
+
+$count = getCounter(DATA_FILE);
+
+
+header('Content-type: application/json');
+echo json_encode([
+      'status' => true
+    , 'count'  => $count
+]);
+
+
+
+function getCounter($file){
+    
+    $fp = fopen($file, 'r+'); 
+    flock($fp, LOCK_EX);      
+    $buff = (int)fgets($fp); 
+
+    
+    ftruncate($fp, 0);    
+    fseek($fp, 0);        
+
+    
+    fwrite($fp, $buff+1);
+
+    
+    flock($fp, LOCK_UN);  
+    fclose($fp);          
+
+    return($buff);
+}
